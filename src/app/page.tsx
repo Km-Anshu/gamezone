d6c7a802761ac9f5c84c2836cced4f161b98bba3
Rohan Khurana <rohan.khurana.dev.iic@gmail.com>
2024-07-25 13:18:21 +0530

    input + buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,20 +44,61 @@ export default function Home() {
   return (
     <>
       <div className="px-10 pb-0 relative  overflow-hidden bg-home-bg bg-cover bg-no-repeat">
-        <input
-          type="text"
-          placeholder="your name"
-          onChange={(e: any) => {
-            setName(e.target.value);
-          }}
-        />
-        <div>
-          <button onClick={SubmitInputInfo}>Submit</button>
+        <div
+          className="p-5 rounded-lg w-[50%]"
+          style={{ margin: "25px auto", background: "lightblue" }}
+        >
+          <h2 className="text-black text-lg font-semibold text-center py-5">
+            Enter Your Name To Start
+          </h2>
+          <input
+            type="text"
+            placeholder="your name"
+            onChange={(e: any) => {
+              setName(e.target.value);
+            }}
+            className="p-2 bg-[#e7e7e7] w-[15rem] flex m-auto"
+            style={{ border: "1px solid #909090;", borderRadius: "8px" }}
+          />
+          <div className="flex m-auto py-5">
+            <button
+              className="text-black flex m-auto"
+              style={{
+                border: "1px solid #909090;",
+                borderRadius: "8px",
+                padding: "8px 20px",
+              }}
+              onClick={SubmitInputInfo}
+            >
+              Submit
+            </button>
+          </div>
         </div>
-        <div>
-          <button onClick={StartPairing}>Start Pairing</button>
+        <div className="flex justify-center items-center">
+          <button
+            className="text-black flex m-auto mr-5"
+            style={{
+              border: "1px solid #909090;",
+              borderRadius: "8px",
+              padding: "8px 20px",
+            }}
+            onClick={StartPairing}
+          >
+            Start Pairing
+          </button>
+
+          <button
+            className="text-black flex m-auto "
+            style={{
+              border: "1px solid #909090;",
+              borderRadius: "8px",
+              padding: "8px 20px",
+            }}
+            onClick={StartGame}
+          >
+            Start Game
+          </button>
         </div>
-        <button onClick={StartGame}>Start Game</button>
       </div>
     </>
   );
